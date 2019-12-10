e98ff2072a7eaadf5f61932301458b4455155bc7
Michael Wali <walimike139@gmail.com>
2019-12-11 00:08:20 +0300

    implement cart checkout functionality

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,6 +3,6 @@ class LineItem < ApplicationRecord
   belongs_to :cart
 
   def total_price
-    instrument.price.to_i * quantity.to_i
+    product.price.to_i * quantity.to_i
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,12 +3,12 @@ class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :category
   has_one_attached :product_image
-  has_many :lineitems
+  has_many :line_items
   # validate :name, :price, :quantity, presence: true
 
   private
 
-  def not_refereced_by_any_line_item
+  def not_referenced_by_any_line_item
     unless line_items.empty?
       errors.add(:base, "Line items present")
       throw :abort
